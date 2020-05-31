28aa3c20eda883cfcb114cdf1b59e64fdc642e3f
ToukiedatakRMTK <66206435+ToukiedatakRMTK@users.noreply.github.com>
2020-05-31 14:15:33 +0200

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,24 @@
+ideologies = [
+    {
+        "name": "Fries",
+        "stats": {
+            "FRL": 100,
+            "REG": 50,
+            "DEM": 50,
+            "COM": 50,
+            "PRO": 50,
+            "ISO": 50
+        }
+    },
+    {
+        "name": "Anti Fries",
+        "stats": {
+            "FRL": 0,
+            "REG": 50,
+            "DEM": 50,
+            "COM": 50,
+            "PRO": 50,
+            "ISO": 50
+        }
+    }
+];
---
+++ questions.js
@@ -0,0 +1,470 @@
+questions = [
+    // friesland vs nederland
+    {
+        "question": "Friesland moet onafhankelijk worden.",
+        "effect": {
+            "FRL": 10,
+            "REG": 5,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Indien Friesland niet onafhankelijk kan worden moet het een speciale status krijgen.",
+        "effect": {
+            "FRL": 10,
+            "REG": 5,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "'t Frysk befrijingsleger doet meer goed dan kwaad.",
+        "effect": {
+            "FRL": 10,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Het classificeren van 't Frysk befrijingsleger als een terroristsche beweging was een terecht besluit.",
+        "effect": {
+            "FRL": -10,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Friesland is en blijft een Nederlandse provincie.",
+        "effect": {
+            "FRL": -10,
+            "REG": -5,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Separatistische organisaties, ook al gebruiken ze geen geweld, moeten hard worden aangepakt.",
+        "effect": {
+            "FRL": -10,
+            "REG": -5,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Er moet meer aandacht komen voor de Friese taal en cultuur.",
+        "effect": {
+            "FRL": 10,
+            "REG": 5,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    // regionaal vs centraal
+    {
+        "question": "Gemeentes en provincies begrijpen hun inwoners beter dan de regering.",
+        "effect": {
+            "FRL": 0,
+            "REG": 10,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Gemeentes en provincies houden zich vaak bezig met onzinnige zaken.",
+        "effect": {
+            "FRL": 0,
+            "REG": -10,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "De gemeentepolitie moet worden heringevoerd.",
+        "effect": {
+            "FRL": 0,
+            "REG": 10,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Politieagenten, ambulancepersoneel en brandweerlieden moeten de lokale streektaal kunnen spreken dan wel verstaan.",
+        "effect": {
+            "FRL": 0,
+            "REG": 10,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Mensen moeten zichzelf zien als Nederlanders ipv uit welke provincie ze komen.",
+        "effect": {
+            "FRL": 0,
+            "REG": -10,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "In elke provincie horen dezelfde wetten te gelden.",
+        "effect": {
+            "FRL": 0,
+            "REG": -10,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Nederland moet een federale staat worden.",
+        "effect": {
+            "FRL": 0,
+            "REG": 10,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    // democratie vs authoritair
+    {
+        "question": "Bindende referenda moeten worden ingevoerd.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 10,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "De leden van de eerste kamer moeten democratisch gekozen worden.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 10,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Volksvertegenwoordigers vertegenwoordigen vaak het volk niet goed.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": -10,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "De meerderheid van de mensen maakt slechte beslissingen.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": -10,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Democratie is de beste bestuursvorm.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 10,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Verkiezingen zijn onzinnig.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": -20,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Nederland moet een absolute monarchie worden.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": -20,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Een coup op z'n tijd moet kunnen.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": -15,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Burgemeesters moeten democratisch gekozen worden.",
+        "effect": {
+            "FRL": 0,
+            "REG": 10,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    // communisme vs kapitalisme
+    {
+        "question": "Onderdrukking door bedrijven is een groter gevaar dan een onderdrukking van de overheid",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 10,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "De rijken moeten meer belasting betalen.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 10,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "De overheid moet niet in de markt ingrijpen.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": -10,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Alle sectoren die door de publieke sector worden uitgevoerd moeten worden vervangen door de private sector.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": -15,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Uitkeringen moeten worden vervangen door liefdadigheidsorganisaties.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": -20,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Arme mensen kiezen ervoor om arm te blijven.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": -15,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "De waarde van een goed moet worden bepaald door de arbeid die in de productie is gestoken.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 15,
+            "PRO": 0,
+            "ISO": 0
+        }
+    },
+    // progressief vs conservatief
+    {
+        "question": "Drugs moet gelegaliseerd dan wel gedecriminaliseerd worden.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 10,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Dierproeven moeten verboden worden.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 10,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "We moeten tradities zoals Sinterklaas behouden, zelfs als sommige mensen erop tegen zijn.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": -10,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Tradities zijn een belangrijk deel van de Nederlandse cultuur.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": -5,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Asielzoekers moeten zoveel mogelijk integreren in de Nederlandse samenleving.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": -10,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Er moeten meer kunst en cultuur subsidies komen.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 10,
+            "ISO": 0
+        }
+    },
+    {
+        "question": "Abortus hoort in vrijwel alle gevallen illegaal te zijn.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": -10,
+            "ISO": 0
+        }
+    },
+    // isolatie vs internationaal
+    {
+        "question": "Nederland moet de Europese Unie verlaten.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 10
+        }
+    },
+    {
+        "question": "De Verenigde Naties heeft meer macht nodig.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": -10
+        }
+    },
+    {
+        "question": "Grenzen moeten afgeschaft worden.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": -10
+        }
+    },
+    {
+        "question": "Globalisme is in de meeste gevallen gunstig.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": -10
+        }
+    },
+    {
+        "question": "We moeten ons niet bezig houden met internationale zaken.",
+        "effect": {
+            "FRL": 0,
+            "REG": 0,
+            "DEM": 0,
+            "COM": 0,
+            "PRO": 0,
+            "ISO": 10
+        }
+    }
+];
